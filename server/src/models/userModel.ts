94433265745df0e79d636c93493f6cc5101447c0
rishishinedev <rishishinedev@gmail.com>
2023-05-16 16:15:40 +0530

    Schema:UserModel

---
+++ server/src/models/userModel.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import validator from 'validator';
+
+interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+}
+
+const userSchema: Schema<IUser> = new Schema<IUser>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+    lowercase: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model<IUser>('User', userSchema);
+export default userModel;
